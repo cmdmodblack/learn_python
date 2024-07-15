cf481941c2af4bc3443ce7c9891ef58bb587c477
cmdmodblack <119146127+cmdmodblack@users.noreply.github.com>
2024-07-15 14:07:22 +0800

    Add files via upload

---
+++ day_01.py
@@ -0,0 +1,23 @@
+'''
+ hello everyone , i'm cmdmod black , these a code for learn python , let's learn it!!!
+'''
+
+# learn Print function
+
+print("hello world!")
+# or
+print('hello world!')
+
+"""
+tip : You can't write it like this:print('you can't write it like this')
+      You can write it like this ： print(“you can't write it like this')
+"""
+# It can also be calculated
+print(1+26)
+''' 
+    it can print 27
+    tip : 1.The operation symbol is +,-,*(multiplication),/,**(power)
+          2.It follows the rules of mathematical operations
+          3.Write the √a as a**(1/2)
+          4.Import Math Library,Import math library, see for usage https://docs.python.org/3/library/math.html
+'''
